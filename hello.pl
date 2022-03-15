2c2867608e1464f053b9843c5b39985e656df8ef
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2022-03-15 15:19:13 -0400

    added built in perl math functions

--- hello.pl
+++ hello.pl
@@ -72,3 +72,12 @@ say "5 / 4 = ", 5 / 4;
 say "5 % 4 = ", 5 % 4;
 # The below will calculate 5 to the power of 4
 say "5 ** 4 = ", 5 ** 4;
+
+# Built-in perl math functions below
+say "EXP 1 = ", exp 1;
+say "HEX 10 = ", hex 10;
+say "OCT 10 = ", oct 10;
+say "INT 6.45 = ", int(6.45);
+say "LOG 2 = ", log 2;
+say "Random between 0 - 10 = ", int(rand 11);
+say "SQRT 9 = ", sqrt 9;
