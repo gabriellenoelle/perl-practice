14039726fd685ee7a1f9e440c177d559c1b9b475
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2022-03-16 11:56:46 -0400

    array practice started and added

---
+++ array_practice.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+my @days = qw(Mon Tue Wed Thu Fri Sat Sun);
+# Below prints each item in the array to the console
+# print("@days" ,"\n");
+
+# Below prints the first item in the array to the console
+print($days[0]);
+print("\n");
+
