a7b1ae6b4946edf837206c450f8b38c855c454d5
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2022-03-16 12:45:06 -0400

    counting arrays practice

--- array_practice.pl
+++ array_practice.pl
@@ -4,9 +4,22 @@ use strict;
 
 my @days = qw(Mon Tue Wed Thu Fri Sat Sun);
 # Below prints each item in the array to the console
-# print("@days" ,"\n");
+print("@days" ,"\n");
 
 # Below prints the first item in the array to the console
+# We use the '$' instead of the '@' because an array element is a scalar
+# Use the prefix that represents what you want to get
 print($days[0]);
 print("\n");
+
+# Below prints the last element in the array
+print($days[-1]);
+
+# Counting Perl array elements
+my $count = scalar @days;
+print($count , "\n");
+
+# The operator '$#' returns the highest index of an array
+my $last = $#days;
+print($last, "\n");
 
