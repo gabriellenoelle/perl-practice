1000b740ff0c343bc98153cdac13561202b44c40
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2022-03-17 13:50:11 -0400

    unshift and sort functions added

--- array_operations.pl
+++ array_operations.pl
@@ -7,6 +7,8 @@ my @stack = (); # empty array
 
 # push() appends one or more elements to the end of an array
 # pop() removes the last element from the end of an array
+# push() and pop() together are based on LIFO (last in first out)
+# like a "stack of books"
 
 print("push 1 to array\n");
 push(@stack,1);
@@ -27,3 +29,44 @@ print("element: $elem\n");
 
 $elem = pop(@stack);
 print("element: $elem\n");
+
+# unshift() and pop() are based on FIFO (first in first out)
+# like a "queue of visitors"
+
+my @queue = (); # empty queue
+print("enqueue 1 to array\n");
+unshift(@queue,1);
+
+print("enqueue 2 to array\n");
+unshift(@queue,2);
+
+printf("enqueue 3 to array\n");
+unshift(@queue,3);
+
+print("@queue", "\n"); # 3 2 1
+
+my $elem = pop(@queue);
+print("element: $elem\n");
+
+$elem = pop(@queue);
+print("element: $elem\n");
+
+$elem = pop(@queue);
+print("element: $elem\n");
+
+# sort() allows you to sort an array in
+# alphabetical or numerical order
+
+my @fruits = qw(oranges apples mango cucumber);
+my @sorted = sort @fruits;
+
+print("@sorted", "\n"); # apples cucumber mango oranges
+
+# sort() also accepts a block of code that allows you to
+# change the sort algorithm
+# to sort an array in numerical order, change the default sorting algorithm
+
+my @a = qw(3 2 1 4 7 6);
+print("unsorted: ", "@a", "\n"); # unsorted: 3 2 1 4 7 6
+@a = sort{$a <=> $b} @a;
+print("sorted: ","@a","\n"); # sorted: 1 2 3 4 6 7
