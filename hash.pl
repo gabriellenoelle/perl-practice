4b282d76f0bb1d9c78573f9d089a449513c75168
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2022-03-18 16:33:46 -0400

    iterating over a hash / hashrefs

--- hash.pl
+++ hash.pl
@@ -19,6 +19,28 @@ Spain => 'Spanish',
 China => 'Chinese',
 Germany => 'German');
 
+# Looking up perl hash values
 # get language of england
 my $lang = $langs{'England'}; # English
 print($lang,"\n");
+
+# Add a new element
+# use a new key-pair
+$langs{'Italy'} = 'Italian';
+
+# Removing a single key/value pair with delete()
+# If you know the hash key, you can remove a single key-value pair
+delete $langs{'China'};
+
+# Modify hash elements
+# add new key value pair
+$langs{'India'} = 'Many languages';
+# modify official language of India
+$langs{'India'} = 'Hindi';
+
+# Loop over perl hash elements with keys()
+# this allows you to get a list of keys in scalars
+# use the keys() function in a for loop to iterate hash elements
+for(keys %langs){
+    print("Official Language of $_ is $langs{$_}\n");
+}
