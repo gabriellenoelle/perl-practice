d23b208c003afdb35826e51d9e3693594e5ff109
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2022-03-23 15:34:29 -0400

    adding perl list practice

---
+++ list.pl
@@ -0,0 +1,11 @@
+# Introduction to Perl List
+# A Perl list is a sequence of scalar values
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+(); # empty list
+(10,20,30); # list of integers
+("this", "is", "a", "list"); #list of strings
+
